400b97ebde957cee74987172e6b4694db8aef5b4
MaxWarAxe <makskriper9@gmail.com>
2024-03-16 15:50:03 +0300

    front commit

---
+++ vue-project/src/components/LectionDialog.vue
@@ -0,0 +1,39 @@
+
+<script setup>
+    import {ref} from 'vue'
+    const props = defineProps({
+        title : String
+    })
+    let active = ref(true)
+</script>
+
+<template>
+    <div class="pa-4 text-center">
+      <v-dialog max-width="1400">
+        <template v-slot:activator="{props: activatorProps }">
+            <v-btn v-bind="activatorProps" color="#60BBFB" icon="../assets/logos/plusIcon.svg" size="small">
+                <img src="../assets/logos/additionIcon.svg" alt="">
+            </v-btn>
+        </template>
+  
+        <template v-slot:default="{ isActive }">
+          <v-card :title="title">
+            <template v-slot:text>
+              Cras pellentesque vel sodales accumsan aenean. Feugiat metus sit nec in aliquet amet, porttitor pretium vulputate massa. Consequat ipsum luctus quisque adipiscing libero. Wisi sollicitudin. Eget vitae ac lobortis, lorem natoque vestibulum et, aliquet faucibus at morbi nibh, vel condimentum. Massa unde orci sed id sed, odio donec congue nec praesent amet. Hymenaeos velit lacus, quis vivamus libero tempus duis, eu nisi eu, ipsum at accumsan pede justo morbi donec, massa et libero sit risus neque tortor. Ut sed sed etiam hendrerit dapibus, quis metus suspendisse nibh.
+            </template>
+  
+            <v-card-actions>
+              <v-spacer></v-spacer>
+  
+              <v-btn
+                color="surface-variant"
+                text="Закрыть"
+                variant="flat"
+                @click="isActive.value = false"
+              ></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    </div>
+  </template>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -9,6 +9,9 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Markdown from 'vue3-markdown-it'
+import 'highlight.js/styles/monokai.css';
+
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import "@mdi/font/css/materialdesignicons.css";
 const vuetify = createVuetify({
@@ -16,4 +19,4 @@ const vuetify = createVuetify({
     directives,
   })
 
-createApp(App).use(vuetify).use(VueAxios,axios).mount('#app')
+createApp(App).use(vuetify).use(VueAxios,axios).use(Markdown).mount('#app')
