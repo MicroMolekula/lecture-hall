0f75e05de42b9e0720155d4ed1fbc5c6d23dbbfd
luke <ivan1.krasikov.2003@gmail.com>
2024-03-16 23:20:23 +0300

    Refactor SubjectController

--- laravel-api/src/app/Http/Controllers/Subject/StoreController.php
+++ laravel-api/src/app/Http/Controllers/Subject/StoreController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\SubjectStoreRequest;
 use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -19,6 +20,13 @@ class StoreController extends Controller
             $teachers = $data['teachers'];
             unset($data['groups'], $data['teachers']);
             $subject = Subject::create($data);
+
+            $count = count($teachers);
+            for ($i = 0; $i < $count; $i += 1){
+                if (User::find($teachers[$i])->role !== 'teacher'){
+                    unset($teachers[$i]);
+                }
+            }
 
             $subject->groups()->attach($groups);
             $subject->users()->attach($teachers);
